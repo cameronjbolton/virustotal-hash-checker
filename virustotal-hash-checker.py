ce85885f50c0cd8166e95bf25b75505e82c29220
cameronjbolton <camjbolt@outlook.com>
2026-02-19 01:18:07 -0600

    Add VirusTotal hash checker script
    This script loads hashes from a CSV file and queries the VirusTotal API for each hash using an API key from environment variables.

---
+++ virustotal-hash-checker.py
@@ -0,0 +1,28 @@
+import csv
+import os
+import requests
+
+def get_virustotal_api_key():
+    if 'VT_API_KEY' not in os.environ:
+        raise Exception("VT_API_KEY environment variable not set. Check system environment variables and try again.")
+    return os.getenv('VT_API_KEY')
+virustotal_api_key = get_virustotal_api_key()
+
+# Load hashes from CSV file
+hashes = []
+with open('hashes.csv', newline='') as csvfile:
+    hashReader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in hashReader:
+        print("Loaded hash: " + row[0])
+        hashes.append(row[0])
+
+# Query VirusTotal for each hash
+for hash in hashes:
+    url = "https://www.virustotal.com/api/v3/files/{hash}"
+    headers = {
+    "accept": "application/json",
+    "x-apikey": virustotal_api_key
+    }
+    response = requests.get(url.format(hash=hash), headers=headers)
+    print(response.text)
+
